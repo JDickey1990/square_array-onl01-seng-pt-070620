44c46f9644e7058aea12b00ec6f4997c3e68dbba
Jordan Dickey <jordan.michael.dickey@gmail.com>
2020-07-11 18:19:04 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
   square_array=["2","4","6"]
   square_array.each.do |array_squared|
-    puts "#{array_squared *array_squared}"
+    puts #{array_squared *array_squared}
   end
 end
