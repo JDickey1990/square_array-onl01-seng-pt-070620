524983d0617a91851cbc1d5bd423d1c9e018e88c
Jordan Dickey <jordan.michael.dickey@gmail.com>
2020-07-11 18:56:35 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   array.each do |number|
    number_squared=number*number
    puts number_squared
-  end
+   
+  en[]
 end
-number_array=[1,2,3]
-square_array(number_array)
+square_array[1,2,3]
